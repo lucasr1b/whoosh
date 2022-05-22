35845f6fb808650ed11393bd282f375cf8f17623
LucasCodes01 <lucascodesweb@gmail.com>
2022-05-22 13:38:09 +1200

    Added promotion banner

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.scss';
 import Navigation from './Navigation';
+import Promotion from './Promotion';
 
 function Home() {
     return (
         <div className='Container'>
             <Navigation />
+            <Promotion />
         </div>
     )
 }
---
+++ src/components/home/Promotion.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Promotion.scss'
+import FoodDelivery from '../../assets/FoodDelivery.png';
+
+function Promotion() {
+  return (
+    <div className='Promotion'>
+        <div className='Promotion--Info'>
+          <h1>Hello Lucas,</h1>
+          <p>Get free delivery with <span>swoosh pro</span> for only <span>$4.99 a month</span></p>
+          <button>Learn More</button>
+        </div>
+        <div className='Promotion--Image'>
+        <img src={FoodDelivery} />
+        </div>
+      </div>
+  )
+}
+
+export default Promotion
