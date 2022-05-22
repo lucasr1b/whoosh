3bc528f64030193f78612b6154954676f278953a
LucasCodes01 <lucascodesweb@gmail.com>
2022-05-22 13:06:10 +1200

    Added order component and made app full height

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/home/Home';
 import './App.scss';
+import Order from './components/order/Order';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Order />
     </div>
   );
 }
---
+++ src/components/order/Order.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Order.scss';
+
+function Order() {
+  return (
+    <div className='Order'>Order</div>
+  )
+}
+
+export default Order
