f34743eb4517a5f5925476b47995282b602d2921
LucasCodes01 <lucascodesweb@gmail.com>
2022-05-22 12:13:14 +1200

    Added home component and added app style

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.scss';
+
+function Home() {
+    return (
+        <div className='Container'>
+            <b>whoosh</b>
+        </div>
+    )
+}
+
+export default Home
