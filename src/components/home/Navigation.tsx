99beaea8eb77656a1721138ce01f4238a1eab198
LucasCodes01 <lucascodesweb@gmail.com>
2022-05-22 12:55:42 +1200

    Added feather icons library and added search icon to search bar

--- src/components/home/Navigation.tsx
+++ src/components/home/Navigation.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Navigation.scss';
+import { Icon } from '@ailibs/feather-react-ts'
 
 function Navigation() {
     return (
         <nav>
             <span className='Brand'>whoosh</span>
-            <div className='Search--Container'><input placeholder='Search by food name' className='Search'/></div>
+            <div className='Search--Container'><input placeholder='Search by food name' className='Search' /><div className='Search--Icon'><Icon name="search" /></div></div>
         </nav>
     )
 }
