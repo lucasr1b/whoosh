7bdd9dfa5238e4089934cac9efcf0c13500e2c37
LucasCodes01 <lucascodesweb@gmail.com>
2022-05-22 16:08:52 +1200

    Added food images and added active prop to MenuCategoryItem

--- src/components/home/Menu.tsx
+++ src/components/home/Menu.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 import './Menu.scss';
 import MenuCategoryItem from './MenuCategoryItem';
 import Burger from '../../assets/food/Burger.png';
+import Pizza from '../../assets/food/Pizza.png';
+import Drink from '../../assets/food/Drink.png';
+import Dessert from '../../assets/food/Dessert.png';
+import Donut from '../../assets/food/Donut.png';
+import Snack from '../../assets/food/Snack.png';
 
 function Menu() {
     return (
         <div className='Menu'>
             <h3>Menu Category</h3>
             <div className='Menu--Category'>
-                <MenuCategoryItem name='Pizza' image={Burger} />
-                <MenuCategoryItem name='Burger' image={Burger} />
-                <MenuCategoryItem name='Drink' image={Burger} />
-                <MenuCategoryItem name='Dessert' image={Burger} />
-                <MenuCategoryItem name='Donut' image={Burger} />
-                <MenuCategoryItem name='Snack' image={Burger} />
+                <MenuCategoryItem name='Burger' image={Burger} active='Menu--Category--Item--Active'/>
+                <MenuCategoryItem name='Pizza' image={Pizza} />
+                <MenuCategoryItem name='Drink' image={Drink} />
+                <MenuCategoryItem name='Dessert' image={Dessert} />
+                <MenuCategoryItem name='Donut' image={Donut} />
+                <MenuCategoryItem name='Snack' image={Snack} />
             </div>
         </div>
     )
--- src/components/home/MenuCategoryItem.tsx
+++ src/components/home/MenuCategoryItem.tsx
@@ -4,7 +4,7 @@ import { Icon } from '@ailibs/feather-react-ts'
 
 function MenuCategoryItem(props: any) {
     return (
-        <div className='Menu--Category--Item'>
+        <div className={'Menu--Category--Item ' + props.active}>
             <div className='Menu--Category--Item--Image'>
                 <img src={props.image}></img>
             </div>
@@ -14,6 +14,10 @@ function MenuCategoryItem(props: any) {
             </div>
         </div>
     )
+}
+
+MenuCategoryItem.defaultProps = {
+    active: ''
 }
 
 export default MenuCategoryItem
