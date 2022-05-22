2b559072f4bc079457926d44ed85da26693dacd6
LucasCodes01 <lucascodesweb@gmail.com>
2022-05-22 17:40:00 +1200

    Rebranded name in promotion banner

--- src/components/home/Promotion.tsx
+++ src/components/home/Promotion.tsx
@@ -7,7 +7,7 @@ function Promotion() {
     <div className='Promotion'>
         <div className='Promotion--Info'>
           <h1>Hello Lucas,</h1>
-          <p>Get free delivery with <span>swoosh pro</span> for only <span>$4.99 a month</span></p>
+          <p>Get free delivery with <span>whoosh pro</span> for only <span>$4.99 a month</span></p>
           <button>Learn More</button>
         </div>
         <div className='Promotion--Image'>
