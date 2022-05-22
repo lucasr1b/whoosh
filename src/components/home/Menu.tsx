baf3f61b337dd8787b7e8eb81ad38edf487abb3c
LucasCodes01 <lucascodesweb@gmail.com>
2022-05-22 17:14:12 +1200

    Added MenuFoodItem component

--- src/components/home/Menu.tsx
+++ src/components/home/Menu.tsx
@@ -7,6 +7,10 @@ import Drink from '../../assets/food/Drink.png';
 import Dessert from '../../assets/food/Dessert.png';
 import Donut from '../../assets/food/Donut.png';
 import Snack from '../../assets/food/Snack.png';
+import MenuFoodItem from './MenuFoodItem';
+import Cheeseburger from '../../assets/display/Cheeseburger.jpg';
+import ChickenBurger from '../../assets/display/ChickenBurger.jpg';
+import BetterBurger from '../../assets/display/BetterBurger.jpg';
 
 function Menu() {
     return (
@@ -19,6 +23,11 @@ function Menu() {
                 <MenuCategoryItem name='Dessert' image={Dessert} />
                 <MenuCategoryItem name='Donut' image={Donut} />
                 <MenuCategoryItem name='Snack' image={Snack} />
+            </div>
+            <div className='Menu--Food'>
+                <MenuFoodItem name='Cheeseburger' price='5.00' rating='5' display={Cheeseburger}/>
+                <MenuFoodItem name='Chicken Burger' price='8.99' rating='4' display={ChickenBurger}/>
+                <MenuFoodItem name='Better Burger' price='6.99' rating='4' display={BetterBurger}/>
             </div>
         </div>
     )
---
+++ src/components/home/MenuFoodItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Icon } from '@ailibs/feather-react-ts'
+
+function MenuFoodItem(props: any) {
+    const rating: any = [];
+
+    for (let i = 0; i < props.rating; i++) {
+        rating.push(i + 1);
+    }
+
+    return (
+        <div className='Menu--Food--Item'>
+            <div className='Menu--Food--Item--Display' style={{background: 'url(' + props.display + ')'}}></div> 
+            <div className='Menu--Food--Item--Details'>
+                <h1>{props.name}</h1>
+                <div className='Rating'>
+                    {rating.map(function (i: any) {
+                        return <Icon name='star' key={i} />
+                    })}
+
+
+                </div>
+                <p><span>$</span>{props.price}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MenuFoodItem
