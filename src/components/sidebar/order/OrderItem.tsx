902700ffe26239709e1e685a38225c32b26e6d3e
LucasCodes01 <lucascodesweb@gmail.com>
2022-06-19 11:33:50 +1200

    Added template item to order menu

--- src/components/sidebar/order/OrderItem.tsx
+++ src/components/sidebar/order/OrderItem.tsx
@@ -5,31 +5,37 @@ import Cheeseburger from '../../../assets/display/Cheeseburger.jpg'
 function OrderItem(props: OrderItemProps) {
 
     return (
-        <div className='Order--Item'>
-            <img src={Cheeseburger} alt='Order Item' />
-            <div className='Order--Item--Details'>
-                {/* <p>Cheeseburger</p>
-                <select>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                    <option>7</option>
-                    <option>8</option>
-                    <option>9</option>
-                </select> */}
-                {props.order.map(o => <div>{o}</div>)}
-            </div>
-            <div className='Order--Item--Price'>
-                <p><span>$</span>5.00</p>
-            </div>
-            <div className='Order--Item--Delete'>
-                <span><Icon name='x' /></span>
-            </div>
+        <>
+            {props.order.map(o => {
+                return (
+                    <div className='Order--Item'>
+                        <img src={Cheeseburger} alt='Order Item' />
+                        <div className='Order--Item--Details'>
+                            <p>{o}</p>
+                            <select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>9</option>
+                            </select>
 
-        </div>
+                        </div>
+                        <div className='Order--Item--Price'>
+                            <p><span>$</span>5.00</p>
+                        </div>
+                        <div className='Order--Item--Delete'>
+                            <span><Icon name='x' /></span>
+                        </div>
+
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
